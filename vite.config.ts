31ec2e665840142a0f90704d2302c7fe7179e6f0
Dmitry <filatov2@finolab.ru>
2025-02-28 00:27:22 +0300

    feat: fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vite.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      vanillaExtractPlugin({
-        identifiers: ({ hash }) => `takeda_${hash}`,
-      }),
-    ],
-    server: {
-      proxy: {
-        '/VITE_DOC_API_URL': {
-          target: process.env.VITE_DOC_API_URL,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/VITE_DOC_API_URL/, ''),
-        },
-        '/VITE_API_URL': {
-          target: process.env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/VITE_API_URL/, ''),
-        },
-      },
-    },
+    plugins: [react(), tsconfigPaths()],
+    base: process.env.VITE_BASE ?? '/',
   });
 };
